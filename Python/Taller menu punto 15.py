44a6b66ca89b055cabedb997432d440fcafc3151
Andres Guerra <andresfg13789@gmail.com>
2023-10-12 16:26:51 -0500

    Taller menu

---
+++ Python/Taller menu punto 15.py
@@ -0,0 +1,22 @@
+#Se tienen dos matrices con datos numericos
+#Formar un diccionario con los primos como clave
+#Y las veces que aparece como valor, ordenarlo por clave
+import random
+matriz1 = [[random.randint(1,20) for j in range(5)] for i in range(5)]
+matriz2 = [[random.randint(1,20) for j in range(5)] for i in range(5)]
+diccionario = {}
+def es_primo(x):
+  if x <= 1:
+    return False
+  for i in range(2,int(x**0.5)+1):
+    if x % i == 0:
+      return False
+  return True
+def agregar_a_diccionario(matriz):
+  primos = []
+  repeticiones = []
+  for i in range(len(matriz)):
+    for j in range(len(matriz)):
+      if es_primo(matriz[i][j]):
+        if matriz[i][j] in primos:
+          primos.append(matriz[i][j] =
