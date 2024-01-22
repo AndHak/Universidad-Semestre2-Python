421589b42ee8865d5d67850685319aa4506e5589
Andres Guerra <andresfg13789@gmail.com>
2024-01-22 01:06:01 -0500

    django6

--- Python y Django/Proyecto1/Proyecto1/views.py
+++ Python y Django/Proyecto1/Proyecto1/views.py
@@ -1,8 +1,32 @@
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
+
+
+class Persona(object):
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
 
 def saludo(request):
-    return HttpResponse("Adios mundo")
+
+    p1 = Persona("Andres", "Guerra", "22")
+
+    ahora = datetime.datetime.now()
+
+    doc_externo = open("C:/Programacion Universidad/Semestre 2/Python y Django/Proyecto1/Proyecto1/Plantillas/Html/plantilla1.html")
+
+    plt = Template(doc_externo.read())
+
+    doc_externo.close()
+
+    ctx = Context({"nombre_persona": p1.nombre, "apellido_persona": p1.apellido, "edad":p1.edad, "hora": ahora})
+
+    documento = plt.render(ctx)
+
+    return HttpResponse(documento)
 
 def despedida(request):
     return HttpResponse("Hola mundo")
