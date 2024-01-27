4312e6d192531a2c3f2f61562eb1bea8b5651fd9
Andres Guerra <andresfg13789@gmail.com>
2024-01-27 00:23:25 -0500

    django 14

---
+++ Python y Django/Tienda online/Tiendaonline/gestionpedidos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionpedidosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestionpedidos'
---
+++ Python y Django/Tienda online/Tiendaonline/gestionpedidos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length = 30)
+    direccion = models.CharField(max_length = 50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length = 10)
+
+class Articulo(models.Model):
+    nombre = models.CharField(max_length = 30)
+    seccion = models.CharField(max_length = 50)
+    precio = models.IntegerField()
+
+class Pedido(models.Model):
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
